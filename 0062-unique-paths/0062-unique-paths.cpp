4690cf48b7e67c87c4c01e8b917d3532f77ca7d7
Aayush kumar shah <aayush.shah.jp@gmail.com>
2026-07-12 00:37:55 +0530

    Time: 3 ms (9.48%), Space: 9.5 MB (30.24%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-//using memoization
-    int path(int row, int col, int m, int n,vector<vector<int>>& dp){
-        if(row >= m || col >= n) return 0;
-        if(row == m-1 && col == n-1) return 1;
+//using tabulation
 
-        if(dp[row][col] != -1) return dp[row][col];
-
-        int right = path(row,col+1,m,n,dp);
-        int down = path(row+1,col,m,n,dp);
-        return dp[row][col] =  right+down;
-    }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int> (n,-1));
-        return path(0,0,m,n,dp);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0) dp[i][j] = 1;
+                else dp[i][j] = dp[i][j-1]+dp[i-1][j];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
