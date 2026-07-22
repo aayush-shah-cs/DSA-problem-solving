12f4c8447cef3c05cf0905cf009feb7f01213988
Aayush kumar shah <aayush.shah.jp@gmail.com>
2026-07-22 14:29:12 +0530

    Time: 2 ms (78.24%), Space: 7.9 MB (38.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
+
+//space Optimization , try to do that in a constant time complexity
+
     int fib(int n) {
-        if(n <= 1) return n;
-        vector<int> dp(n+1);
-    //iterative method ->top down
-        dp[0] = 0, dp[1] = 1;
+
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        vector<int> dp(3);
+        dp[0] = 0;
+        dp[1] = 1;
 
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[2] = dp[0]+dp[1];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
         }
-        return dp[n];
+        return dp[2];
     }
 };
