4b625ae552d6f281733225f388c3e510908f9d07
Aayush kumar shah <aayush.shah.jp@gmail.com>
2026-07-24 09:55:56 +0530

    Time: 0 ms (100%), Space: 32.5 MB (50.33%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int alternatingSum(vector<int>& nums) {
+        int odd_sum = 0;
+        int even_sum = 0;
+        for(int i=0;i<nums.size();i++){
+            if(i%2 == 0) even_sum += nums[i];
+            else odd_sum += nums[i];
+        }
+        return even_sum - odd_sum;
+    }
+};
