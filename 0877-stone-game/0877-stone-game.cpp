a37b9a09f794135e1f300bf64e2d3158bedc6710
Aayush kumar shah <aayush.shah.jp@gmail.com>
2026-08-02 15:10:33 +0530

    Time: 21 ms (7.99%), Space: 19.6 MB (20.08%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
+    int whoWins(vector<int>& piles, int i,int j, vector<vector<int>>& dp){
+        if(i == j) return piles[i];
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int takeleft_side = piles[i] - whoWins(piles,i+1,j,dp);
+        int takeright_side = piles[j] - whoWins(piles,i,j-1,dp);
+        return dp[i][j] = max(takeleft_side, takeright_side); 
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+        return whoWins(piles,0,n-1,dp) > 0;
     }
 };
