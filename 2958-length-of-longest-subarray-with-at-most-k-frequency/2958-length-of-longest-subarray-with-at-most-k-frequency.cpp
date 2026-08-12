1c193a726be96b087520c738740b46f14f1307c4
Aayush kumar shah <aayush.shah.jp@gmail.com>
2026-08-12 21:36:06 +0530

    Time: 60 ms (73.54%), Space: 149.3 MB (84.27%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -3,20 +3,17 @@ public:
     int maxSubarrayLength(vector<int>& nums, int k) {
         unordered_map<int,int> freq;
         int left = 0;
-
-        int longest_subarr = 0;
-
-        for(int right=0;right<nums.size();right++){
+        int right = 0;
+        int ans = 0;
+        while(right < nums.size()){
             freq[nums[right]]++;
-
-                while(freq[nums[right]] > k){
-                    freq[nums[left]]--;
-                    left++;
-
-                }
-            
-            longest_subarr = max(longest_subarr, right - left + 1);
+            while(freq[nums[right]] > k){
+                freq[nums[left]]--;
+                left++;
+            }
+            ans = max(ans,right-left+1);
+            right++;
         }
-        return longest_subarr;
+        return ans;
     }
 };
