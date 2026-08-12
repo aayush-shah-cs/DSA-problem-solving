c1a0b2fdb58fde095deb677b78e261025edd637d
Aayush kumar shah <aayush.shah.jp@gmail.com>
2026-08-12 15:54:30 +0530

    Time: 229 ms (5.03%), Space: 194.1 MB (5.85%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,41 +1,45 @@
 class MedianFinder {
 public:
-    multiset<int> st;
-    multiset<int>::iterator mid;
-    int n = 0;
+    multiset<int> mt1, mt2;
+
     MedianFinder() {}
-    
+
     void addNum(int num) {
-        if(st.empty()){
-            st.insert(num);
-            mid = st.begin();
-            n++;
+        // Initially put the number into mt2
+        mt2.insert(num);
+
+        // If mt2 has an element smaller than the largest
+        // element of mt1, swap them
+        if (!mt1.empty() && !mt2.empty()) {
+            int first = *mt2.begin();
+            int last = *mt1.rbegin();
+
+            if (first < last) {
+                mt2.erase(mt2.find(first));
+                mt1.erase(mt1.find(last));
+
+                mt1.insert(first);
+                mt2.insert(last);
+            }
         }
-        else{
-            st.insert(num);
-            if(n%2 == 0 && num >= (*mid)){
-                mid++;
-            }
-            if(n%2 != 0 && num < (*mid)){
-                mid--;
-            }
-            n++;
+
+        // Keep mt1 size >= mt2 size
+        if (mt2.size() > mt1.size()) {
+            int curr = *mt2.begin();
+
+            mt2.erase(mt2.find(curr));
+            mt1.insert(curr);
         }
     }
-    
+
     double findMedian() {
-        if(n%2 == 0){
-            auto it = mid;
-            it++;
-            return ((*mid)+ (*it))/2.0;
+        if (mt1.size() > mt2.size()) {
+            return *mt1.rbegin();
         }
-        return *(mid);
+
+        double left = *mt1.rbegin();
+        double right = *mt2.begin();
+
+        return (left + right) / 2.0;
     }
 };
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
