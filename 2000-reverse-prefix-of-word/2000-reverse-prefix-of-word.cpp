b5c792671e399418e0206be6b47458c2c63cfbed
Aayush kumar shah <aayush.shah.jp@gmail.com>
2026-09-01 00:15:52 +0530

    Time: 0 ms (100%), Space: 8.2 MB (90.38%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
+    string reversing(string& part,int end){
+        int i = 0;
+        while(i < end){
+            char temp = part[i];
+            part[i] = part[end];
+            part[end] = temp;
+            i++;
+            end--;
+        }
+        return part;
+    }
     string reversePrefix(string word, char ch) {
-        int end=0;
-        while(end < word.length()){
-            if(ch == word[end]){
-                reverse(word.begin(),word.begin()+end+1);
-                break;
+        for(int i=0;i<word.length();i++){
+            if(ch == word[i]){
+               return reversing(word,i);
             }
-            end++;
         }
         return word;
     }
