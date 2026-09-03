1c3e22f914d2eaf46f67d9ee029453d6d66de1cd
Aayush kumar shah <aayush.shah.jp@gmail.com>
2026-09-04 01:46:00 +0530

    Time: 3 ms (61.74%), Space: 18.3 MB (82.72%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -16,14 +16,13 @@ public:
         if(!list2) return list1;
 
         if(list1->val < list2->val){
-            list1->next = mergeTwoLists(list1->next,list2);
+            list1->next = mergeTwoLists(list1->next, list2);
             return list1;
         }
         else{
-            list2->next = mergeTwoLists(list1,list2->next);
+            list2->next = mergeTwoLists(list1, list2->next);
             return list2;
         }
-        return list1;
     }
     
     ListNode* EachIndex(vector<ListNode*>& lists,int i,int j){
@@ -39,6 +38,6 @@ public:
 
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if(lists.empty()) return nullptr;
-        return EachIndex(lists,0,lists.size());
+        return EachIndex(lists,0,lists.size()-1);
     }
 };
