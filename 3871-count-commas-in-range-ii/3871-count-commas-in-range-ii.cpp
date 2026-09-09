170a368dbc86efaf313e614460919c550133cf1c
Aayush kumar shah <aayush.shah.jp@gmail.com>
2026-09-09 10:50:52 +0530

    Time: 0 ms (100%), Space: 9.1 MB (19.2%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+            for (long long base = 1000; base <= n; base *= 1000) {
+                ans += (n - base + 1);
+            }
+        return ans;
+    }
+};
