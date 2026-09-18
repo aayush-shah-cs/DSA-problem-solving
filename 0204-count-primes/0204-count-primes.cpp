8c92efdf57a71927f52d7a65e0b2f3b23a3080f2
Aayush kumar shah <aayush.shah.jp@gmail.com>
2026-09-18 10:13:07 +0530

    Time: 683 ms (19.76%), Space: 13.5 MB (47.52%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,40 +1,19 @@
 class Solution {
 public:
     int countPrimes(int n) {
-
-        if (n <= 2)
-            return 0;
-
-        vector<bool> prime(n, true);
-
-        prime[0] = false;
-        prime[1] = false;
-
-        // All even numbers greater than 2 are not prime
-        for (int i = 4; i < n; i += 2) {
-            prime[i] = false;
-        }
-
-        // Only check odd numbers
-        for (long long i = 3; i * i < n; i += 2) {
-
-            if (prime[i]) {
-
-                // Only mark odd multiples
-                for (long long j = i * i; j < n; j += 2 * i) {
-                    prime[j] = false;
-                }
+        if(n <= 2) return 0;
+        bool arr[n];
+        memset(arr, true, n);
+        for(int i = 2; i * i < n; i++){
+            if(!arr[i]) continue;
+            for(int j = i * i; j < n; j+=i){
+                arr[j] = false;
             }
         }
-
-        int count = 0;
-
-        for (int i = 2; i < n; i++) {
-            if (prime[i]) {
-                count++;
-            }
+        int count = 0; 
+        for(int i = 2; i < n; i++){
+            if(arr[i]) count++;
         }
-
         return count;
     }
 };
