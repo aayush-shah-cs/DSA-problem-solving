5aab73ac888c2781a5d5ed9f5da65ca2a87206bc
Aayush kumar shah <aayush.shah.jp@gmail.com>
2026-09-23 05:38:12 +0530

    Time: 4 ms (14.01%), Space: 17.3 MB (16.83%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,47 +1,55 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        //concept of multi sourcse BFS
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        queue<vector<int>> q;
-        int count = 0;
-
-        vector<int> dr = {1, -1, 0, 0};
-        vector<int> dc = {0, 0, -1, 1};
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    q.push({i, j, 0});
-                    vis[i][j] = 1;
-                } else if (grid[i][j] == 1) {
-                    count++;
-                }
-            }
-        }
-
         int ans = 0;
-        while (!q.empty()) {
-            int r = q.front()[0];
-            int c = q.front()[1];
-            int time = q.front()[2];
+
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+
+        queue<pair<pair<int,int>,int>> q; // ({{i,j},0})
+        // we storing all the occurence of the 2 in the grid and will perform multi source BFS
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                if(grid[i][j] == 2)
+                    q.push({{i,j},0});
+
+        
+        while(!q.empty()){
+            int i = q.front().first.first;
+            int j = q.front().first.second;
+            int time = q.front().second;
             q.pop();
-            
-            ans = time;
 
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
+            ans = max(ans,time);
 
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == 1 && vis[nr][nc] == 0) {
-                    vis[nr][nc] = 1;
-                    q.push({nr, nc, time + 1});
-                    count--;
+            if(i-1 >= 0 && grid[i-1][j] == 1 && !vis[i-1][j]){
+                q.push({{i-1,j},time+1});
+                vis[i-1][j] = true;
+            }
+            if(i+1 < n && grid[i+1][j] == 1 && !vis[i+1][j]){
+                q.push({{i+1,j},time+1});
+                vis[i+1][j] = true;
+            }
+            if(j-1 >= 0 && grid[i][j-1] == 1 && !vis[i][j-1]){
+                q.push({{i,j-1},time+1});
+                vis[i][j-1] = true;
+            }
+            if(j+1 < m && grid[i][j+1] == 1 && !vis[i][j+1]){
+                q.push({{i,j+1},time+1});
+                vis[i][j+1] = true;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 1 && !vis[i][j]){
+                    return -1;
                 }
             }
         }
 
-        return count == 0 ? ans : -1;
+        return ans;
     }
 };
